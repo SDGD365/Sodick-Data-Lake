338d56d1cd7172a0ec091bb13f85b73b578b4384
Stephan Weichers <IT.Administration@sodick.de>
2026-03-05 13:09:02 +0100

    Updates

--- Program.cs
+++ Program.cs
@@ -1,19 +1,9 @@
-using System.Text.Json;
 using Microsoft.Azure.Functions.Worker;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var host = new HostBuilder()
-  .ConfigureFunctionsWebApplication()
-  .ConfigureServices(services =>
-  {
-      services.Configure<JsonSerializerOptions>(o =>
-      {
-          o.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-          o.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
-          o.PropertyNameCaseInsensitive = true;
-      });
-  })
-  .Build();
+    .ConfigureFunctionsWebApplication()
+    .ConfigureServices(services => {})
+    .Build();
 
 host.Run();
--- obj/Debug/net8.0/WorkerExtensions/obj/Release/net8.0/WorkerExtensions.AssemblyInfo.cs
+++ obj/Debug/net8.0/WorkerExtensions/obj/Release/net8.0/WorkerExtensions.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Microsoft.Azure.Functions.Worker.Extensions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e42ff9e66b5695c5f4bba2e47184b6459427cc5e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+337e1931097532bb9b3dee88371c745e87d9fe32")]
 [assembly: System.Reflection.AssemblyProductAttribute("Microsoft.Azure.Functions.Worker.Extensions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Microsoft.Azure.Functions.Worker.Extensions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
